df0a54766121aad69b39f2fb3c28e49f52a0e88d
Alexu12345 <135076423+Alexu12345@users.noreply.github.com>
2024-08-30 17:29:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 function Cl() {
     document.getElementById('bttn').style.backgroundColor='rgb(17, 186, 17)';
+    let Code = document.getElementById('ctl00_cntphmaster_txt_StudCode')
     setTimeout(() => {
         document.getElementById('bttn').value = 'جاري التحقق من الرقم القومي';
     }, 500);
     setTimeout(() => {
+        if(Code.value == 30205080200655 || '30205080200655'){
         document.getElementById('Mr').style.display='none';
         document.getElementById('box-body').style.display='none';
         document.getElementById('Mn').style.display='block';
+        } else{
+            document.getElementById('bttn').value = 'خطأ في البيانات';
+            document.getElementById('bttn').style.backgroundColor='red';
+        }
     }, 2000);
 }
